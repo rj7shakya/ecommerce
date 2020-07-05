45b9f3254b5483b2c759901f6319ef895407985a
rj7shakya <shakyarajad1@gmail.com>
2020-07-05 12:46:38 +0545

    base for peoject setup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/layout/Navbar";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import Home from "./components/pages/Home";
-import Shop from "./components/pages/Shop";
-import Cart from "./components/pages/Cart";
-import About from "./components/pages/About";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <div className="container-fluid">
-          <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/shop" component={Shop} />
-          <Route exact path="/cart" component={Cart} />
-          <Route exact path="/about" component={About} />
-          <Redirect to="/" />
-        </div>
-      </Switch>
-    </Router>
-  );
+  return <div>hello world</div>;
 }
 
 export default App;
