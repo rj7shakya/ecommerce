2ce511187cd7143d2282a5f80c4c6459e627bc4c
rj7shakya <shakyarajad1@gmail.com>
2020-07-23 08:48:45 +0545

    header and shop page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
