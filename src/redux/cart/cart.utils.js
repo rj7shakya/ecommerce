b1af9bb08b8ca87b2c978ac7072ef3985c9f7396
rj7shakya <shakyarajad1@gmail.com>
2020-11-01 18:40:16 +0545

    checkout page complete

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,18 @@ export const addItemToCart = (cartItems, cartitemToAdd) => {
 
   return [...cartItems, { ...cartitemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartitemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartitemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartitemToRemove.id);
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartitemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
