10de27675371c0a2fe7361cbd84523a5201d970e
rj7shakya <shakyarajad1@gmail.com>
2020-11-01 14:30:15 +0545

    cart item added in redux

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,15 +2,21 @@ import { CartActionTypes } from './cart.types';
 
 const INTIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INTIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
-      console.log('called');
       return {
         ...state,
         hidden: !state.hidden,
+      };
+
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
       };
 
     default:
