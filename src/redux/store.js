d22fc1d41cb4f724887fa1d47520b802954e5807
rj7shakya <shakyarajad1@gmail.com>
2020-11-07 18:29:29 +0545

    redux persistance added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { logger } from 'redux-logger';
 import { applyMiddleware, createStore } from 'redux';
+import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
